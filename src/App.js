2954402d8f4d8984fdf2a969e6b6553f6e94381f
moe_dude90 <mohammed.elsharif@gmail.com>
2017-05-15 10:56:20 +0100

    -Deployed and implement METACOIN contract for Money Transfer -Simple UI interface to transfer money -Log to all transfer transactions

--- src/App.js
+++ src/App.js
@@ -8,60 +8,133 @@ import metacoin_artifacts from '../build/contracts/MetaCoin.json'
 
 //Set up web3 provider
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//Set Default Account to be the Owner of contracts, in the case of truffle the first account
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
+let Balance =0;
+var EuroToken = contract(metacoin_artifacts);
+EuroToken.setProvider(web3.currentProvider);
+//console.log(EuroToken);
 
-var MetaCoin = contract(metacoin_artifacts);
-MetaCoin.setProvider(web3.currentProvider);
-
-console.log(MetaCoin);
-MetaCoin.deployed().then(function(instance) {
+let balancePromise = EuroToken.deployed().then(function(instance) {
   let meta = instance;
-  return instance.getBalance.call(web3.eth.accounts[0]);
-}).then(function(balance) {
-  console.log(balance.valueOf());
+ // console.log(meta);
+  return meta.getBalance.call(web3.eth.accounts[0]);
+}).then(function(meta) {
+//  console.log(meta);
+  Balance = meta.valueOf();
+//  console.log(Balance);
+   return Balance;
+});
+
+let TransPromise = EuroToken.deployed().then(function(instance) {
+  let meta = instance;
+ // console.log(meta);
+  return meta.allEvents({fromBlock: 0, toBlock: 'latest', event: 'Transfer'});
+}).then(function(events) {
+  return new Promise((resolve, reject)=>{
+    const res = events.get(function(error, result){
+     if (error)
+      reject(error);
+      else
+      resolve(result);
+
+    });
+  })
 
 });
 
-var accounts;
-var account;
+class NameForm extends React.Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+  async componentDidMount() {
+    const balance = await balancePromise;
+    this.setState({balance, account: web3.eth.accounts[0]});
+  }
 
-web3.eth.getAccounts(function(err, accs) {
-     if (err != null) {
-       alert("There was an error fetching your accounts.");
-       return;
-     }
+  render(){
+    return (<h3>Your account:
+      <br></br>&ensp;&ensp;
+        {this.state.account} Balance: {this.state.balance}
+      </h3>)
+  }
+}
 
-     if (accs.length == 0) {
-       alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-       return;
-     }
+class Transfer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {User: '', Amount: ''};
 
-     accounts = accs;
-     account = accounts[0];
-    // console.log(account);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleAmountChange(event) {
+    this.setState({Amount: event.target.value});
+  }
+  handleUserChange(event) {
+    this.setState({User: event.target.value});
+  }
 
-});
-//console.log(account);
-const NameForm = () => (
-      <h3>Your account:
-        <br></br>&ensp;&ensp;
-          {web3.eth.accounts[0]}
-        </h3>
-    );
+  handleSubmit(event) {
+  //  alert(this.state.Amount +'\n'+ this.state.User);
+    let amount = this.state.Amount;
+    let addrs = this.state.User;
+    EuroToken.deployed().then(function(instance) {
+      let meta = instance;
 
-const Deposit = () => (
+      return meta.sendCoin(addrs,amount);
+    }).then(function(suc) {
+      var test=9;
+//      console.log(suc);
+    });
+
+  }
+
+  render() {
+    return (
       <h3>Make Depositt:
         <form>
         <br></br>
-          client:  <input type="text"/>
+          client:  <input type="text" value={this.state.User} onChange={this.handleUserChange}/>
         <br></br>
-          Amount:  <input type="text"/> nEur <input type="submit" value="Send"/>
+          Amount:  <input type="text" value={this.state.Amount} onChange={this.handleAmountChange}/> nEur
+            <input type="submit" value="Send" onClick={this.handleSubmit}/>
         </form>
       </h3>
-);
+    );
+  }
+}
 
-const Withdraw =() => (<h3>Withdraws:</h3>);
+class Withdraw extends React.Component {
+  constructor(){
+    super();
+  //  this.state = {Type : '', To: '', Amount: ''}
+  this.state = {list: ''};
+  }
+  async componentDidMount() {
+    const Transactions = await TransPromise;
+    console.log(Transactions);
+
+    ///let test = this.state.Type.concat(Transactions[1].event);
+    var list = Transactions.map((Transactions) => {
+      return [<dir>{Transactions.event} to {Transactions.args._to} {Transactions.args._value.c[0]} nEuro</dir>];
+    });
+    this.setState({list});
+    console.log(this.state.list);
+
+  }
+
+  render(){
+
+    return (<h3>Withdraws:<br></br>
+        <ul>{this.state.list}</ul></h3>)
+  }
+}
+
+//const Withdraw =() => (<h3>Withdraws:</h3>);
 
 class App extends Component {
   render() {
@@ -72,7 +145,7 @@ class App extends Component {
             <h2>Neufund</h2>
         </div>
           <div><NameForm/></div>
-          <div><Deposit/></div>
+          <div><Transfer/></div>
           <Withdraw/>
         </div>
     );
